df66093af9a939bf18d7eee069a589d6293b7e24
Joao Neves <joaoneves792@gmail.com>
2017-04-01 13:44:41 +0100

    Integrated raytracer with webserver

---
+++ README.md
@@ -0,0 +1,20 @@
+## RenderFarm
+
+# WebServer
+
+Compile raytracer with make
+
+Compile WebServer with:
+
+javac WebServer.java -cp ./raytracer-master/src/
+
+Run Webserver with:
+
+java -cp ./raytracer-master/src:. WebServer
+
+Examples of valid requests:
+
+http://localhost:8000/r.html?f=test05.txt&sc=400&sr=300&wc=400&wr=300&coff=0&roff=0
+
+http://localhost:8000/test
+
--- WebServer/WebServer.java
+++ WebServer/WebServer.java
@@ -1,12 +1,15 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executor;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import raytracer.RayTracer;
 
 public class WebServer {
 
@@ -16,16 +19,17 @@ public class WebServer {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/test", new TestHandler());
+        server.createContext("/r.html", new RenderHandler());
         server.setExecutor(java.util.concurrent.Executors.newFixedThreadPool(THREAD_COUNT)); // creates a default executor
         server.start();
     }
 
-    static class MyHandler implements HttpHandler {
+    static class TestHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
             //String response = "This was the query:" + t.getRequestURI().getQuery()+ "##";
-            String response = ""+WebServer.counter.getAndIncrement();
+            String response = "Server is up.";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -33,13 +37,46 @@ public class WebServer {
         }
     }
 
-    private static class RenderExecutor implements Executor {
-        public void execute (Runnable task){
-            task.run();
+
+    static class RenderHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response;
+            Map<String, String> arguments = new HashMap<>();
+            String query = t.getRequestURI().getQuery();
+            for(String arg : query.split("&")){
+                String pair[] = arg.split("=");
+                if (pair.length > 1){
+                    arguments.put(pair[0], pair[1]);
+                }
+            }
+
+            try {
+                File inFile = new File("raytracer-master/"+ arguments.get("f"));
+                File outFile = new File(counter.getAndIncrement() + ".bmp");
+
+                //TODO we are not checking if these actually exist
+                int sc = Integer.parseInt(arguments.get("sc"));
+                int sr = Integer.parseInt(arguments.get("sr"));
+                int wc = Integer.parseInt(arguments.get("wc"));
+                int wr = Integer.parseInt(arguments.get("wr"));
+                int coff = Integer.parseInt(arguments.get("coff"));
+                int roff = Integer.parseInt(arguments.get("roff"));
+
+                RayTracer raytracer = new RayTracer(sc, sr, wc, wr, coff, roff);
+                raytracer.readScene(inFile);
+                raytracer.draw(outFile);
+            }catch (Exception e){
+                response = e.getMessage();
+                System.out.println(response);
+            }
+            response = "Rendering finished!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         }
-
     }
-
 }
 
 
